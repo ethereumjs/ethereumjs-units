6d8e64081d5ec1d732b4194490ffac08b8c90e73
Alex Beregszaszi <alex@rtfs.hu>
2016-04-13 22:33:20 +0100

    Remove dependency on bignumber
    Fixes #3

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-var BigNumber = require('bignumber.js')
-
 var Units = {}
 
 var rawUnits = require('./units.json')
 var units = {}
 
 Object.keys(rawUnits).map(function (unit) {
-  units[unit] = new BigNumber(rawUnits[unit], 10)
+  // drop the leading number 1
+  units[unit] = rawUnits[unit].length - 1
 })
 
 Units.units = rawUnits
+
+// add leading zeroes
+// FIXME: improve
+function zeroes (n) {
+  var tmp = ''
+  for (var i = 0; i < n; i++) {
+    tmp += '0'
+  }
+  return tmp
+}
+
+// strip leading zeroes
+// FIXME: improve
+function strip (n) {
+  return n.match(/^0*([0-9]+)$/)[1]
+}
+
+// strip trailing decimal zeroes
+// FIXME: improve
+function striptail (n) {
+  var tmp = n.match(/(0*)$/)[1].length
+  if (tmp > 0) {
+    return n.slice(0, -tmp)
+  } else {
+    return n
+  }
+}
+
+// this will always multiply to wei and thus no decimal point will be included
+function mul (value, scale) {
+  var tmp = value.split('.')
+  value = tmp[0]
+  var frac = tmp[1]
+
+  if (tmp.length === 2) {
+    frac = frac.slice(0, scale)
+  } else {
+    frac = ''
+  }
+
+  return strip(value + frac + zeroes(scale - frac.length))
+}
+
+function div (value, scale) {
+  if (scale === 0 || value === '0') {
+    return value
+  } else if (value.length > scale) {
+    return striptail(value.slice(0, -scale) + '.' + value.slice(scale))
+  } else {
+    return striptail('0.' + zeroes(scale - value.length) + value)
+  }
+}
 
 var re = RegExp(/^[0-9]+\.?[0-9]*$/)
 
@@ -19,16 +70,16 @@ Units.convert = function (value, from, to) {
   }
 
   from = from.toLowerCase()
-  if (!units[from]) {
+  if (typeof units[from] === 'undefined') {
     throw new Error('Unsupported input unit')
   }
 
   to = to.toLowerCase()
-  if (!units[to]) {
+  if (typeof units[to] === 'undefined') {
     throw new Error('Unsupported output unit')
   }
 
-  return new BigNumber(value, 10).mul(units[from]).round(0, BigNumber.ROUND_DOWN).div(units[to]).toString(10)
+  return div(mul(value, units[from]), units[to])
 }
 
 Units.lazyConvert = function (value, to) {
